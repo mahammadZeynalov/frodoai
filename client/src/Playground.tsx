d1b842b4b340ed7aea3d1b184bb4e129b5ef4b38
Mahammad Zeynalov <mzeynalov@ebrevia.com>
2024-09-03 21:47:12 +0400

    game logic changes (adapt to chat)

--- client/src/Playground.tsx
+++ client/src/Playground.tsx
@@ -1,174 +1,94 @@
 /* eslint-disable @typescript-eslint/no-use-before-define */
 /* eslint-disable no-console */
 import "./App.css";
-import { IProvider } from "@web3auth/base";
 import { useEffect, useState } from "react";
-import { Database } from "@tableland/sdk";
-import { GALADRIEL_CONFIG, TABLE_NAME } from "./consts";
-import { Contract, ethers, Wallet } from "ethers";
-import { AnswerType, Game, GameStatus } from "./models";
+import { Contract, ethers, TransactionReceipt } from "ethers";
 import { useWeb3Auth } from "@web3auth/modal-react-hooks";
+import { galadriel, GALADRIEL_CONFIG } from "./consts";
 
-const provider = new ethers.JsonRpcProvider(GALADRIEL_CONFIG.rpcUrl);
-const wallet = new Wallet(GALADRIEL_CONFIG.privateKey, provider);
-const contract = new Contract(
-  GALADRIEL_CONFIG.contractAddress,
-  GALADRIEL_CONFIG.abi,
-  wallet
-);
+interface Message {
+  role: string;
+  content: string;
+}
 
 function Playground() {
   const [walletAddress, setWalletAddress] = useState<string>("");
-  const [dbClient, setDbClient] = useState<Database>();
-  const [isQuestionLoading, setIsQuestionLoading] = useState(false);
   const [currentAnswer, setCurrentAnswer] = useState("");
-  const [isAnswerLoading, setIsAnswerLoading] = useState(false);
-  const [activeGame, setActiveGame] = useState<Game | null>(null);
   const [isGameCreationLoading, setIsGameCreationLoading] = useState(false);
   const { initModal, provider, web3Auth, isConnected, connect, logout } =
     useWeb3Auth();
 
-  const initDbClient = async (provider: IProvider) => {
-    const ethersProvider = new ethers.BrowserProvider(provider);
-    const signer = await ethersProvider.getSigner();
-    const db = new Database({ signer });
-    setDbClient(db);
-  };
-
-  const getUserGames = async () => {
-    if (dbClient) {
-      const response: any = await dbClient
-        .prepare(
-          `SELECT * FROM ${TABLE_NAME} where wallet_address='${walletAddress}';`
-        )
-        .all();
-      const games = response.results as Game[];
-      console.log("user games: ", games);
-      const activeGame = games.find((i) => i.status === GameStatus.IN_PROGRESS);
-      console.log("active game: ", activeGame);
-      if (activeGame) {
-        setActiveGame(activeGame);
+  const initGame = async (auto: boolean) => {
+    let chatId: any;
+    if (auto) {
+      chatId = localStorage.getItem("chatId");
+      if (chatId) {
+        chatId = Number(chatId);
+      } else {
+        chatId = createChat();
       }
+      initChatLoop(chatId);
+    } else {
+      chatId = createChat();
+      initChatLoop(chatId);
     }
   };
 
-  const startNewGame = async () => {
+  const createChat = async () => {
+    // Call the startChat function
     setIsGameCreationLoading(true);
-    const { meta: insert } = await dbClient!
-      .prepare(
-        `INSERT INTO ${TABLE_NAME} (wallet_address, status) VALUES (?, ?);`
-      )
-      .bind(walletAddress, GameStatus.IN_PROGRESS)
-      .run();
-    const response = await insert.txn?.wait();
-    console.log("Create game response: ", response);
-    setIsGameCreationLoading(false);
-    // Getting first question
-    getQuestion();
-  };
-
-  const setQuestionInDb = async (npcName: string, questionText: string) => {
-    const updateQuery = `
-          UPDATE ${TABLE_NAME}
-          SET npc_name = '${npcName}',
-          current_question_text = '${questionText}'
-          WHERE wallet_address='${walletAddress}';
-    `;
-    return dbClient!.prepare(updateQuery).run();
-  };
-
-  const getQuestion = async () => {
-    console.log("Init question: ", GALADRIEL_CONFIG.promptToAskQuestion);
-    setIsQuestionLoading(true);
-    const transactionResponse = await contract.sendMessage(
+    const transactionResponse = await galadriel.startChat(
       GALADRIEL_CONFIG.promptToAskQuestion
     );
     const receipt = await transactionResponse.wait();
+    setIsGameCreationLoading(false);
     console.log(`Message sent, tx hash: ${receipt.hash}`);
     console.log(
       `Chat started with message: "${GALADRIEL_CONFIG.promptToAskQuestion}"`
     );
 
-    // Read the LLM response on-chain
+    // Get the chat ID from transaction receipt logs
+    const chatId = getChatId(receipt, galadriel);
+
+    console.log(`Created chat ID: ${chatId}`);
+    if (!chatId && chatId !== 0) {
+      return;
+    }
+    localStorage.setItem("chatId", chatId.toString());
+    return chatId;
+  };
+
+  const initChatLoop = async (chatId: number) => {
+    const allMessages: Message[] = [];
+    // Run the chat loop: read messages and send messages
     while (true) {
-      const current_question_text: string = await contract.response();
-      console.log(current_question_text);
-      if (current_question_text) {
-        console.log("Galadriel ask following question:", current_question_text);
-        console.log("updateGame start");
-        const setQuestionInDbResponse = await setQuestionInDb(
-          "Bilbo",
-          current_question_text
-        );
-        console.log("setQuestionInDbResponse: ", setQuestionInDbResponse);
-        setIsQuestionLoading(false);
-        // should update values TODO
-        setActiveGame((prev) => ({
-          ...prev!,
-          npc_name: "Bilbo",
-          current_question_text,
-        }));
-        break;
+      const newMessages: Message[] = await getNewMessages(
+        galadriel,
+        chatId,
+        allMessages.length
+      );
+      if (newMessages) {
+        for (const message of newMessages) {
+          console.log(`${message.role}: ${message.content}`);
+          allMessages.push(message);
+          if (allMessages.at(-1)?.role === "assistant") {
+            const message = currentAnswer;
+            const transactionResponse = await galadriel.addMessage(
+              message,
+              chatId
+            );
+            const receipt = await transactionResponse.wait();
+            console.log(`Message sent, tx hash: ${receipt.hash}`);
+          }
+        }
       }
       await new Promise((resolve) => setTimeout(resolve, 2000));
     }
   };
 
-  const updateCounterInDb = async (isPreviousAnswerCorrect: boolean) => {
-    const fields: string[] = [];
-    const updateQuestionNumber = "question_number = question_number + 1";
-    fields.push(updateQuestionNumber);
-    const updateCorrectAnswered = isPreviousAnswerCorrect
-      ? "correct_answered = correct_answered + 1"
-      : "";
-    if (updateCorrectAnswered) {
-      fields.push(updateCorrectAnswered);
-    }
-    const updateStatus =
-      activeGame?.question_number === 3
-        ? `status = '${GameStatus.COMPLETED}'`
-        : "";
-    if (updateStatus) {
-      fields.push(updateStatus);
-    }
-    const str = fields.join(", ");
-    console.log(fields);
-    const updateQuery = `
-          UPDATE ${TABLE_NAME}
-          SET ${str}
-          WHERE wallet_address='${walletAddress}';
-    `;
-    console.log("query: ", updateQuery);
-    return dbClient!.prepare(updateQuery).run();
-  };
+  const getQuestion = async () => {};
 
-  const answerQuestion = async (answer: string) => {
-    setIsAnswerLoading(true);
-    const transactionResponse = await contract.sendMessage(
-      `You asked me following question: ${activeGame?.current_question_text}. My answer is ${answer}. If the answer is correct send CORRECT. If the answer is not correct send NOT_CORRECT. Do not send anything besides CORRECT or NOT_CORRECT.`
-    );
-    await transactionResponse.wait();
-
-    // Read the LLM response on-chain
-    while (true) {
-      const response: AnswerType = await contract.response();
-      console.log(response);
-      if (response) {
-        setIsAnswerLoading(false);
-        console.log("Your answer is:", response);
-        const isCorrectAnswer = response === AnswerType.CORRECT;
-        const updateCounterInDbResponse = await updateCounterInDb(
-          isCorrectAnswer
-        );
-        console.log("updateCounterInDbResponse: ", updateCounterInDbResponse);
-        setCurrentAnswer("");
-        // should update values TODO
-        setActiveGame((prev) => ({ ...prev!, current_question_text: "" }));
-        break;
-      }
-      await new Promise((resolve) => setTimeout(resolve, 2000));
-    }
-  };
+  const answerQuestion = async (answer: string) => {};
 
   useEffect(() => {
     const init = async () => {
@@ -193,30 +113,20 @@ function Playground() {
   useEffect(() => {
     if (isConnected && provider) {
       const getAddress = async () => {
-        console.log("called");
         const ethersProvider = new ethers.BrowserProvider(provider);
         const signer = await ethersProvider.getSigner();
         const address = await signer.getAddress();
         setWalletAddress(address);
       };
       getAddress();
-      initDbClient(provider);
     }
   }, [isConnected, provider]);
 
   useEffect(() => {
-    if (dbClient) {
-      getUserGames();
+    if (isConnected && walletAddress) {
+      initGame(true);
     }
-  }, [dbClient]);
-
-  useEffect(() => {
-    if (activeGame?.status === GameStatus.COMPLETED) {
-      console.log("Game completed.");
-    } else if (activeGame && !activeGame.current_question_text) {
-      getQuestion();
-    }
-  }, [activeGame]);
+  }, [isConnected, walletAddress]);
 
   const loggedInView = (
     <>
@@ -224,49 +134,6 @@ function Playground() {
       <div className="mt-3">
         Help Frodo to ask questions and burn ring in Morder!
       </div>
-
-      {!activeGame && (
-        <div className="mt-3">
-          <button
-            type="button"
-            className="btn btn-primary"
-            onClick={() => startNewGame()}
-            disabled={isGameCreationLoading}
-          >
-            Start Game
-          </button>
-        </div>
-      )}
-
-      {activeGame && (
-        <div className="mt-3">
-          <div className="question">
-            {isQuestionLoading ? (
-              <span>Loading...</span>
-            ) : (
-              <h5>{activeGame.current_question_text}</h5>
-            )}
-          </div>
-          <div className="mt-3">
-            <div>
-              <label className="form-label">Answer: </label>
-              <input
-                type="text"
-                className="form-control"
-                value={currentAnswer}
-                onChange={(event) => setCurrentAnswer(event.target.value)}
-                disabled={isQuestionLoading || isAnswerLoading}
-              />
-            </div>
-            <button
-              className="btn btn-primary mt-2"
-              onClick={() => answerQuestion(currentAnswer)}
-            >
-              Submit answer
-            </button>
-          </div>
-        </div>
-      )}
     </>
   );
 
@@ -298,3 +165,39 @@ function Playground() {
 }
 
 export default Playground;
+
+function getChatId(receipt: TransactionReceipt, contract: Contract) {
+  let chatId;
+  for (const log of receipt.logs) {
+    try {
+      const parsedLog = contract.interface.parseLog(log);
+      if (parsedLog && parsedLog.name === "ChatCreated") {
+        // Second event argument
+        chatId = ethers.toNumber(parsedLog.args[1]);
+      }
+    } catch (error) {
+      // This log might not have been from your contract, or it might be an anonymous log
+      console.log("Could not parse log:", log);
+    }
+  }
+  return chatId;
+}
+
+async function getNewMessages(
+  contract: Contract,
+  chatId: number,
+  currentMessagesCount: number
+): Promise<Message[]> {
+  const messages = await contract.getMessageHistory(chatId);
+
+  const newMessages: Message[] = [];
+  messages.forEach((message: any, i: number) => {
+    if (i >= currentMessagesCount) {
+      newMessages.push({
+        role: message[0],
+        content: message.content[0].value,
+      });
+    }
+  });
+  return newMessages;
+}
--- client/src/consts/index.ts
+++ client/src/consts/index.ts
@@ -1,4 +1,5 @@
 import GALADRIEL_ABI from "../assets/ChatGpt.json";
+import { Contract, ethers, Wallet } from "ethers";
 
 export const GALADRIEL_CONFIG = {
   rpcUrl: "https://devnet.galadriel.com/",
@@ -9,4 +10,11 @@ export const GALADRIEL_CONFIG = {
     "Ask me short question that relates to geography. Formulate the question the way, so the answer for it will be one word.",
   abi: GALADRIEL_ABI,
 };
-export const TABLE_NAME = "game_table_11155111_1799";
+
+const provider = new ethers.JsonRpcProvider(GALADRIEL_CONFIG.rpcUrl);
+const wallet = new Wallet(GALADRIEL_CONFIG.privateKey, provider);
+export const galadriel = new Contract(
+  GALADRIEL_CONFIG.contractAddress,
+  GALADRIEL_CONFIG.abi,
+  wallet
+);
