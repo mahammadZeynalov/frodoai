839716025c581d002b1b3f0848ca3d6dcf46273f
Mahammad Zeynalov <mzeynalov@ebrevia.com>
2024-09-04 15:59:32 +0400

    add nft minter contract

--- client/src/consts/index.ts
+++ client/src/consts/index.ts
@@ -3,7 +3,8 @@ import { Contract, ethers, Wallet } from "ethers";
 
 export const GALADRIEL_CONFIG = {
   rpcUrl: "https://devnet.galadriel.com/",
-  contractAddress: "0xE8775D4b4F016F0ED9Cc30B6fF604676371E8457",
+  chatContractAddress: "0xE8775D4b4F016F0ED9Cc30B6fF604676371E8457",
+  nftContractAddress: "0xC59Df6A81bB91F71a08C092d8Bb21664498ceB01",
   privateKey:
     "19d271c33253cbcc08c63025cc3fbefd6e9f0af875079518d5a4078187705049",
   abi: GALADRIEL_ABI,
@@ -12,7 +13,7 @@ export const GALADRIEL_CONFIG = {
 const provider = new ethers.JsonRpcProvider(GALADRIEL_CONFIG.rpcUrl);
 const wallet = new Wallet(GALADRIEL_CONFIG.privateKey, provider);
 export const galadriel = new Contract(
-  GALADRIEL_CONFIG.contractAddress,
+  GALADRIEL_CONFIG.chatContractAddress,
   GALADRIEL_CONFIG.abi,
   wallet
 );
