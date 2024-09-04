edb988aef423d4b701d154695e8889d0b4d7d68d
Mahammad Zeynalov <mzeynalov@ebrevia.com>
2024-09-04 11:01:52 +0400

    add prompt file

--- client/src/consts/index.ts
+++ client/src/consts/index.ts
@@ -6,8 +6,6 @@ export const GALADRIEL_CONFIG = {
   contractAddress: "0x77dAC2431273c5200E243e044EAfBBcB6A21069D",
   privateKey:
     "19d271c33253cbcc08c63025cc3fbefd6e9f0af875079518d5a4078187705049",
-  promptToAskQuestion:
-    "Ask me short question that relates to geography. Formulate the question the way, so the answer for it will be one word.",
   abi: GALADRIEL_ABI,
 };
 
